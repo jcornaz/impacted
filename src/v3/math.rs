c4c24f4af98a1caf4567fc9faf6f21313a4cb6fc
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2023-08-04 15:22:42 +0200

    v3: implement mul and div for vector

--- src/v3/math.rs
+++ src/v3/math.rs
@@ -1,4 +1,4 @@
-use core::ops::{Add, AddAssign, Sub, SubAssign};
+use core::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
 
 #[derive(Debug, Copy, Clone, Default, PartialEq)]
 pub struct Vec2 {
@@ -41,6 +41,36 @@ impl Sub for Vec2 {
     type Output = Self;
     fn sub(mut self, rhs: Self) -> Self::Output {
         self -= rhs;
+        self
+    }
+}
+
+impl MulAssign<f32> for Vec2 {
+    fn mul_assign(&mut self, rhs: f32) {
+        self.x *= rhs;
+        self.y *= rhs;
+    }
+}
+
+impl Mul<f32> for Vec2 {
+    type Output = Self;
+    fn mul(mut self, rhs: f32) -> Self::Output {
+        self *= rhs;
+        self
+    }
+}
+
+impl DivAssign<f32> for Vec2 {
+    fn div_assign(&mut self, rhs: f32) {
+        self.x /= rhs;
+        self.y /= rhs;
+    }
+}
+
+impl Div<f32> for Vec2 {
+    type Output = Self;
+    fn div(mut self, rhs: f32) -> Self::Output {
+        self /= rhs;
         self
     }
 }
@@ -101,6 +131,22 @@ mod tests {
         assert_abs_diff_eq!(v1 - v2, expected);
         let mut res = v1;
         res -= v2;
+        assert_abs_diff_eq!(res, expected);
+    }
+
+    #[rstest]
+    #[case(Vec2::new(0.0, 0.0), 1.0, Vec2::new(0.0, 0.0))]
+    #[case(Vec2::new(1.0, 0.0), 2.0, Vec2::new(2.0, 0.0))]
+    #[case(Vec2::new(0.0, 1.0), 2.0, Vec2::new(0.0, 2.0))]
+    #[case(Vec2::new(2.0, 3.0), 4.0, Vec2::new(8.0, 12.0))]
+    fn test_mul_div(#[case] v: Vec2, #[case] f: f32, #[case] expected: Vec2) {
+        assert_abs_diff_eq!(v * f, expected);
+        assert_abs_diff_eq!(v / f.recip(), expected);
+        let mut res = v;
+        res *= f;
+        assert_abs_diff_eq!(res, expected);
+        res = v;
+        res /= f.recip();
         assert_abs_diff_eq!(res, expected);
     }
 }
