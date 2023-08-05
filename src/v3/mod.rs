09af352e36da91abaad7d6bc48a13cc16a501b10
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2023-08-05 15:07:38 +0200

    v3: implement range collision check

--- src/v3/mod.rs
+++ src/v3/mod.rs
@@ -36,6 +36,13 @@ impl Range {
     }
 }
 
+#[sealed]
+impl Collides<Range> for Range {
+    fn collides(&self, other: &Range) -> bool {
+        self.min <= other.max && self.max >= other.min
+    }
+}
+
 #[cfg(test)]
 impl approx::AbsDiffEq for Range {
     type Epsilon = f32;
@@ -44,5 +51,35 @@ impl approx::AbsDiffEq for Range {
     }
     fn abs_diff_eq(&self, other: &Self, epsilon: Self::Epsilon) -> bool {
         self.min.abs_diff_eq(&other.min, epsilon) && self.max.abs_diff_eq(&other.max, epsilon)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case((0.0, 1.0), (0.5, 1.5))]
+    #[case((0.0, 1.0), (1.0, 1.5))]
+    #[case((0.0, 1.0), (-1.0, 0.5))]
+    #[case((0.0, 1.0), (-1.0, 0.0))]
+    #[case((0.0, 1.0), (0.1, 0.9))]
+    #[case((0.0, 1.0), (-1.0, 2.0))]
+    #[case((0.0, 0.0), (0.0, 0.0))]
+    fn range_should_overlap(#[case] r1: (f32, f32), #[case] r2: (f32, f32)) {
+        let r1 = Range::from_min_max(r1.0, r1.1);
+        let r2 = Range::from_min_max(r2.0, r2.1);
+        assert!(r1.collides(&r2), "{r1:?} does not overlap {r2:?}");
+    }
+
+    #[rstest]
+    #[case((0.0, 1.0), (1.1, 1.5))]
+    #[case((2.0, 3.0), (0.0, 1.0))]
+    #[case((2.0, 3.0), (0.0, 1.0))]
+    fn range_should_not_overlap(#[case] r1: (f32, f32), #[case] r2: (f32, f32)) {
+        let r1 = Range::from_min_max(r1.0, r1.1);
+        let r2 = Range::from_min_max(r2.0, r2.1);
+        assert!(!r1.collides(&r2), "{r1:?} overlaps {r2:?}");
     }
 }
