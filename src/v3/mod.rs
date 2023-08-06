80f4cd110e8b9c215fb9121529a5991674cc5012
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2023-08-06 21:09:14 +0200

    v3: work on ray cast implementation

--- src/v3/mod.rs
+++ src/v3/mod.rs
@@ -21,16 +21,6 @@ struct Contact {
     point: Point,
 }
 
-fn cast_ray(_origin: Point, vector: Vec2, target: &impl SatShape) -> Option<Contact> {
-    let projection = target.project(vector);
-    if projection.min > 1.0 {
-        return None;
-    }
-    Some(Contact {
-        point: Vec2::new(projection.min, 0.0).into(),
-    })
-}
-
 #[cfg(test)]
 mod tests {
     use crate::v3::shapes::Aabb;
@@ -39,12 +29,35 @@ mod tests {
     use approx::assert_abs_diff_eq;
     use rstest::rstest;
 
+    fn cast_ray(origin: Point, vector: Vec2, target: &impl SatShape) -> Option<Contact> {
+        let projection = target.project(vector).min;
+        if !(0.0..=1.0).contains(&projection) {
+            return None;
+        }
+        Some(Contact {
+            point: Vec2::new(projection + origin.project(vector).min, 0.0).into(),
+        })
+    }
+
     #[rstest]
     #[case(
         Vec2::ZERO,
         Vec2::X,
         Aabb::from_size(Vec2::new(2.0, 2.0)).with_position(Vec2::new(1.5, 0.0)),
         Vec2::new(0.5, 0.0)
+    )]
+    #[case(
+        Vec2::new(10.0, 0.0),
+        Vec2::X,
+        Aabb::from_size(Vec2::new(2.0, 2.0)).with_position(Vec2::new(1.5, 0.0)),
+        Vec2::new(10.5, 0.0)
+    )]
+    #[ignore = "not implemented"]
+    #[case(
+        Vec2::ZERO,
+        Vec2::X * 2.0,
+        Aabb::from_size(Vec2::new(2.0, 2.0)).with_position(Vec2::new(2.5, 0.0)),
+        Vec2::new(1.5, 0.0)
     )]
     fn ray_cast_should_find_contact_point(
         #[case] origin: impl Into<Point>,
@@ -57,12 +70,16 @@ mod tests {
     }
 
     #[rstest]
-    fn ray_cast_should_return_none_when_there_is_no_hit() {
-        let result = cast_ray(
-            Vec2::ZERO.into(),
-            Vec2::X,
-            &Aabb::from_size(Vec2::new(1.0, 1.0)).with_position(Vec2::new(1.6, 0.0)),
-        );
+    #[case(Vec2::ZERO, Vec2::X, Aabb::from_size(Vec2::new(1.0, 1.0)).with_position(Vec2::new(1.6, 0.0)))]
+    #[case(Vec2::ZERO, Vec2::X, Aabb::from_size(Vec2::new(1.0, 1.0)).with_position(Vec2::new(-1.6, 0.0)))]
+    #[ignore = "not implemented"]
+    #[case(Vec2::X * 10.0, Vec2::X, Aabb::from_size(Vec2::new(1.0, 1.0)).with_position(Vec2::new(0.5, 0.0)))]
+    fn ray_cast_should_return_none_when_there_is_no_hit(
+        #[case] origin: impl Into<Point>,
+        #[case] vector: Vec2,
+        #[case] target: impl SatShape,
+    ) {
+        let result = cast_ray(origin.into(), vector, &target);
         assert_eq!(result, None);
     }
 }
