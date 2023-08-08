2f160e004d6c36240b9e1050ddfb59f63867c5e8
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2023-08-08 06:30:00 +0200

    v3: support ray cast in other directions

--- src/v3/mod.rs
+++ src/v3/mod.rs
@@ -38,7 +38,7 @@ mod tests {
             return None;
         }
         Some(Contact {
-            point: Vec2::new(projected_target, 0.0).into(),
+            point: (normal * projected_target).into(),
         })
     }
 
@@ -61,6 +61,37 @@ mod tests {
         Aabb::from_size(Vec2::new(2.0, 2.0)).with_position(Vec2::new(2.9, 0.0)),
         Vec2::new(1.9, 0.0)
     )]
+    #[case(
+        Vec2::ZERO,
+        Vec2::Y,
+        Aabb::from_size(Vec2::new(2.0, 2.0)).with_position(Vec2::new(0.0, 1.9)),
+        Vec2::new(0.0, 0.9)
+    )]
+    #[case(
+        Vec2::Y,
+        Vec2::Y,
+        Aabb::from_size(Vec2::new(2.0, 2.0)).with_position(Vec2::new(0.0, 2.9)),
+        Vec2::new(0.0, 1.9)
+    )]
+    #[case(
+        Vec2::ZERO,
+        Vec2::Y * 2.0,
+        Aabb::from_size(Vec2::new(2.0, 2.0)).with_position(Vec2::new(0.0, 2.9)),
+        Vec2::new(0.0, 1.9)
+    )]
+    #[case(
+        Vec2::ZERO,
+        Vec2::new(1.0, 1.0),
+        Aabb::from_size(Vec2::new(2.0, 2.0)).with_position(Vec2::new(1.9, 1.9)),
+        Vec2::new(0.9, 0.9),
+    )]
+    #[ignore = "not implemented"]
+    #[case(
+        Vec2::ZERO,
+        Vec2::new(1.0, 1.0),
+        Aabb::from_size(Vec2::new(2.0, 2.0)).with_position(Vec2::new(0.5, 1.9)),
+        Vec2::new(0.9, 0.9),
+    )]
     fn ray_cast_should_find_contact_point(
         #[case] origin: impl Into<Point>,
         #[case] vector: Vec2,
@@ -76,6 +107,10 @@ mod tests {
     #[case(Vec2::ZERO, Vec2::X, Aabb::from_size(Vec2::new(2.0, 2.0)).with_position(Vec2::new(-2.1, 0.0)))]
     #[case(Vec2::ZERO, Vec2::X, Aabb::from_size(Vec2::new(2.0, 2.0)).with_position(Vec2::ZERO))]
     #[case(-Vec2::X, Vec2::X, Aabb::from_size(Vec2::new(2.0, 2.0)).with_position(Vec2::new(1.1, 0.0)))]
+    #[ignore = "not implemented"]
+    #[case(Vec2::ZERO, Vec2::X, Aabb::from_size(Vec2::new(2.0, 2.0)).with_position(Vec2::new(1.9, 5.0)))]
+    #[ignore = "not implemented"]
+    #[case(Vec2::ZERO, Vec2::X, Aabb::from_size(Vec2::new(2.0, 2.0)).with_position(Vec2::new(1.9, -5.0)))]
     fn ray_cast_should_return_none_when_there_is_no_hit(
         #[case] origin: impl Into<Point>,
         #[case] vector: Vec2,
